04b2f3ccb7f4a88e3de870e096f2702dd5c843ca
Radha Kumari <56878565+12345radha@users.noreply.github.com>
2025-08-29 09:04:20 +0530

    Time: 4 ms (100%), Space: 43.9 MB (88.92%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+
+        int n=x;
+        int res=0;
+        while(n>0)
+        {
+            res=res*10+(n%10);
+           n/=10;
+        }
+
+        return (res==x)?true:false;
+
+
+    }
+}
