f49f9c4743dafdae8ef8e9a692efe2e596b362cb
Radha Kumari <56878565+12345radha@users.noreply.github.com>
2025-09-20 22:28:18 +0530

    Time: 827 ms (9.1%), Space: 45.7 MB (14.39%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,28 +1,25 @@
 class Solution {
-    private boolean ispallin(String s)
+    private boolean isPallin(String s)
     {
-        char[] a=s.toCharArray();
         int i=0,j=s.length()-1;
         while(i<=j)
         {
-            if(a[i]!=a[j])return false;
+            if(s.charAt(i)!=s.charAt(j))return false;
             i++;
             j--;
         }
         return true;
     }
     public int countSubstrings(String s) {
-      int cnt=0;
+        int cnt=0;
         for(int i=0;i<s.length();i++)
         {
             for(int j=i;j<s.length();j++)
             {
-                if(ispallin(s.substring(i,j+1)))cnt++;
-
+                if(isPallin(s.substring(i,j+1)))
+                cnt++;
             }
-            
         }
         return cnt;
-
     }
 }
