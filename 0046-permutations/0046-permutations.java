f6d3a20d939771096d2f116f4cafd0d186e1a7b6
Radha Kumari <56878565+12345radha@users.noreply.github.com>
2025-09-29 22:23:18 +0530

    Time: 1 ms (92.78%), Space: 44.6 MB (46.09%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void swap(int[] nums,int i,int j)
+    {
+        int x=nums[i];
+        nums[i]=nums[j];
+        nums[j]=x;
+    }
+
+    public void display(int[] nums,int start,int end,List<List<Integer>> res)
+    {
+        if(start==end){
+            List<Integer> temp=new ArrayList<>();
+            for(int x:nums)
+            temp.add(x);
+            res.add(temp);
+            return;
+        }
+
+        for(int i=start;i<=end;i++)
+        {
+            swap(nums,start,i);
+            display(nums,start+1,end,res);
+             swap(nums,i,start);
+        }
+    }
+
+    
+    public List<List<Integer>> permute(int[] nums) 
+    {
+         List<List<Integer>> res=new ArrayList<>();
+         
+        display(nums, 0,nums.length-1,res);
+        return res;
+        
+    }
+}
